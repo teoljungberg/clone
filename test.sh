b1634df6ec58acd3dd507a5ed5f20006a502e6ef
Teo Ljungberg <teo@teoljungberg.com>
2023-12-28 21:50:13 +0100

    Make test-cases less author specific

--- test.sh
+++ test.sh
@@ -15,19 +15,19 @@ test_case() {
 echo "Cloning variations of URLs in pwd:"
 echo "pwd: $(pwd)"
 cd "$ROOT" || exit 1
-test_case "git@github.com:teoljungberg/dotfiles"
-test_case "git@github.com:teoljungberg/dotfiles.git"
-test_case "teoljunberg/dotfiles"
-test_case "dotfiles"
-test_case "git@git.sr.ht:~teoljungberg/dotfiles"
-test_case "git@git.sr.ht:~teoljungberg/dotfiles.git"
+test_case "git@github.com:user/project"
+test_case "git@github.com:user/project.git"
+test_case "another-user/project"
+test_case "project"
+test_case "git@git.sr.ht:~user/project"
+test_case "git@git.sr.ht:~user/project.git"
 
 echo "Cloning outside of project base directory:"
 TMPDIR=$(mktemp -d) || exit 1
 (
   cd "$TMPDIR" || exit 1
   echo "pwd: $(pwd)"
-  test_case "git@github.com:teoljungberg/dotfiles"
-  test_case "git@git.sr.ht:~teoljungberg/dotfiles"
+  test_case "git@github.com:user/project"
+  test_case "git@git.sr.ht:~user/project"
 )
 rmdir "$TMPDIR"
