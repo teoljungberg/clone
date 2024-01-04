9579506fe7d62d6408f3180c6e755a7d476e2717
Teo Ljungberg <teo@teoljungberg.com>
2024-01-04 13:53:34 +0100

    Extract smaller testcases

--- tests/clone-inside-project-base-directory.sh
+++ tests/clone-inside-project-base-directory.sh
@@ -1,30 +1,39 @@
-if testcase "clone inside project base directory"; then
-  cd "$ROOT" || exit 1
+cd "$ROOT" || exit 1
 
+if testcase "clone full git+ssh github.com URLs inside clone's directory structure"; then
   assert_eq \
     "git clone git@github.com:user/project $HOME/src//github.com/user/project" \
     "$(clone "git@github.com:user/project")"
   assert_eq \
     "git clone git@github.com:user/project $HOME/src//github.com/user/project" \
     "$(clone "git@github.com:user/project.git")"
+fi
 
-  assert_eq \
-    "git clone git@github.com:another-user/project $HOME/src//github.com/another-user/project" \
-    "$(clone "another-user/project")"
-  assert_eq \
-    "git clone git@github.com:another-user/project $HOME/src//github.com/another-user/project" \
-    "$(clone "another-user/project.git")"
-  assert_eq \
-    "git clone git@github.com:teoljungberg/project $HOME/src//github.com/teoljungberg/project" \
-    "$(clone "project")"
-  assert_eq \
-    "git clone git@github.com:teoljungberg/project $HOME/src//github.com/teoljungberg/project" \
-    "$(clone "project.git")"
-
+if testcase "clone full git+ssh git.sr.ht URLs inside clone's directory structure"; then
   assert_eq \
     "git clone git@git.sr.ht:~user/project $HOME/src//git.sr.ht/~user/project" \
     "$(clone "git@git.sr.ht:~user/project")"
   assert_eq \
     "git clone git@git.sr.ht:~user/project $HOME/src//git.sr.ht/~user/project" \
     "$(clone "git@git.sr.ht:~user/project.git")"
+fi
+
+if testcase "clone user/repository name patterns inside clone's directory structure"; then
+  assert_eq \
+    "git clone git@github.com:another-user/project $HOME/src//github.com/another-user/project" \
+    "$(clone "another-user/project")"
+  assert_eq \
+    "git clone git@github.com:another-user/project $HOME/src//github.com/another-user/project" \
+    "$(clone "another-user/project.git")"
+fi
+
+if testcase "clone repository_name patterns inside a user's clone directory structure"; then
+  user=$(basename $(dirname $ROOT))
+
+  assert_eq \
+    "git clone git@github.com:$user/project $HOME/src//github.com/$user/project" \
+    "$(clone "project")"
+  assert_eq \
+    "git clone git@github.com:$user/project $HOME/src//github.com/$user/project" \
+    "$(clone "project.git")"
 fi
--- tests/clone-outside-project-base-directory.sh
+++ tests/clone-outside-project-base-directory.sh
@@ -1,20 +1,22 @@
-if testcase "clone outside project base directory"; then
-  TMPDIR=$(mktemp -d) || exit 1
-  cd "$TMPDIR" || exit 1
+tmpdir=$(mktemp -d) || exit 1
+cd "$tmpdir" || exit 1
 
+if testcase "clone full git+ssh github.com URLs outside clone's directory structure"; then
   assert_eq \
     "git clone git@github.com:user/project $HOME/src//github.com/user/project" \
     "$(clone "git@github.com:user/project")"
   assert_eq \
     "git clone git@github.com:user/project $HOME/src//github.com/user/project" \
     "$(clone "git@github.com:user/project.git")"
+fi
 
+if testcase "clone full git+ssh git.sr.ht URLs outside clone's directory structure"; then
   assert_eq \
     "git clone git@git.sr.ht:~user/project $HOME/src//git.sr.ht/~user/project" \
     "$(clone "git@git.sr.ht:~user/project")"
   assert_eq \
     "git clone git@git.sr.ht:~user/project $HOME/src//git.sr.ht/~user/project" \
     "$(clone "git@git.sr.ht:~user/project.git")"
-
-  rmdir "$TMPDIR"
 fi
+
+rmdir "$tmpdir"
