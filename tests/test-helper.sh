44a40f6e475bd28f8db8506fb2406fa16f6099e1
Teo Ljungberg <teo@teoljungberg.com>
2024-05-09 21:49:00 +0200

    Inline variable

--- tests/test-helper.sh
+++ tests/test-helper.sh
@@ -4,9 +4,8 @@ set -e
 
 ROOT=$(cd "$(dirname "$0")/.." && pwd)/
 # shellcheck disable=SC2034
-CLONE="$ROOT/clone"
 
 clone() {
   # redirect stderr to stdout
-  "$CLONE" -n "$@" 2>&1
+  "$ROOT/clone" -n "$@" 2>&1
 }
