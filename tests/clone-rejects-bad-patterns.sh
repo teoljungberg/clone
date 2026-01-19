b62dff1a064dc7b48409a9ea8060ff40383a0723
Teo Ljungberg <teo@teoljungberg.com>
2026-01-19 17:11:04 +0100

    Add tests for path traversal rejection
    Test that path traversal sequences (.., /) are rejected in:
    - hostname field
    - user field
    - repository name field

--- tests/clone-rejects-bad-patterns.sh
+++ tests/clone-rejects-bad-patterns.sh
@@ -15,3 +15,30 @@ if testcase "clone rejects bad patterns - ssh://"; then
     "clone: could not extract repository: ssh://git@github.com:user/project.git" \
     "$(clone "ssh://git@github.com:user/project.git")"
 fi
+
+if testcase "clone rejects path traversal in hostname"; then
+  assert_eq \
+    "clone: could not extract repository: git@../../etc:user/repo" \
+    "$(clone "git@../../etc:user/repo")"
+  assert_eq \
+    "clone: could not extract repository: git@foo/../bar:user/repo" \
+    "$(clone "git@foo/../bar:user/repo")"
+fi
+
+if testcase "clone rejects path traversal in user"; then
+  assert_eq \
+    "clone: could not extract repository: git@github.com:../user/repo" \
+    "$(clone "git@github.com:../user/repo")"
+  assert_eq \
+    "clone: could not extract repository: https://github.com/../user/repo" \
+    "$(clone "https://github.com/../user/repo")"
+fi
+
+if testcase "clone rejects path traversal in repository name"; then
+  assert_eq \
+    "clone: could not extract repository: git@github.com:user/../repo" \
+    "$(clone "git@github.com:user/../repo")"
+  assert_eq \
+    "clone: could not extract repository: git@github.com:user/repo/../other" \
+    "$(clone "git@github.com:user/repo/../other")"
+fi
